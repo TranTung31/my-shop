4deede400a036680418252ddbfe7a4286786651f
Your Name <you@example.com>
2023-10-10 23:53:00 +0700

    13 - Tích hợp react-query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  // useEffect(() => {
+  //   fetch();
+  // }, []);
+  const fetchAPI = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API}/product/get-all`);
+    return res.data;
+  }
+  const query = useQuery({ queryKey: ['todos'], queryFn: fetchAPI });
+  console.log('query', query);
   return (
     <div>
       <Router>
